2e769ab321eeb742254bd2026b94f32c38bbf258
Whovian85 <harold.stewart@zx-ventures.com>
2018-08-31 16:20:57 -0400

    Add files via upload

---
+++ Eclipse/action_page.php
@@ -0,0 +1,74 @@
+<?php
+/* Set e-mail recipient */
+$myemail  = "harold.stewart@zx-ventures.com";
+
+/* Check all form inputs using check_input function */
+$name = $_POST['name'];
+$company   = check_input($_POST['company']);    
+$phone = check_input($_POST['phone'], "Enter your phone");
+$email    = check_input($_POST['email']);
+$comments = check_input($_POST['comments'], "Write your comments");
+
+/* If e-mail is not valid show error message */
+if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email))
+{
+    show_error("E-mail address not valid");
+}
+
+/* If URL is not valid set $website to empty */
+if (!preg_match("/^(https?:\/\/+[\w\-]+\.[\w\-]+)/i", $website))
+{
+    $website = '';
+}
+
+/* Let's prepare the message for the e-mail */
+$message = "Hello!
+
+Your contact form has been submitted by:
+
+Your Name: $name
+Your Company: $company
+Your Phone: $phone
+E-mail: $email
+Comments: $comments
+
+
+
+End of message
+";
+
+/* Send the message using mail() function */
+mail($myemail, $subject, $message);
+
+/* Redirect visitor to the thank you page */
+header('Location: thanks.html');
+exit();
+
+/* Functions we used */
+function check_input($data, $problem='')
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    if ($problem && strlen($data) == 0)
+    {
+        show_error($problem);
+    }
+    return $data;
+}
+
+function show_error($myError)
+{
+?>
+    <html>
+    <body>
+
+    <b>Please correct the following error:</b><br />
+    <?php echo $myError; ?>
+
+    </body>
+    </html>
+<?php
+exit();
+}
+?>
---
+++ Eclipse/join_page.php
@@ -0,0 +1,75 @@
+<?php
+/* Set e-mail recipient */
+$myemail  = "harold.stewart@zx-ventures.com";
+
+/* Check all form inputs using check_input function */
+$company = check_input($_POST['company']);
+$companyemail   = check_input($_POST['companyemail']);    
+$carbonreduction = check_input($_POST['carbonreduction']);
+$goal = check_input($_POST['goal']);
+
+
+/* If e-mail is not valid show error message */
+if (!preg_match("/([\w\-\.]+\@[\w\-]+\.[\w\-]+)/", $companyemail))
+{
+    echo $companyemail;
+    show_error("E-mail address not valid");
+}
+
+/* If URL is not valid set $website to empty */
+if (!preg_match("/^(https?:\/\/+[\w\-]+\.[\w\-]+)/i", $website))
+{
+    $website = '';
+}
+
+/* Let's prepare the message for the e-mail */
+$message = "Hello!
+
+Your contact form has been submitted by:
+
+COMPANY NAME: $company
+COMPANY EMAIL: $companyemail
+SUSTAINABILITY: $carbonreduction
+HOW WILL YOU ACHIEVE THIS GOAL?: $goal
+
+
+
+
+End of message
+";
+
+/* Send the message using mail() function */
+mail($myemail, $subject, $message);
+
+/* Redirect visitor to the thank you page */
+header('Location: thanks.html');
+exit();
+
+/* Functions we used */
+function check_input($data, $problem='')
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    if ($problem && strlen($data) == 0)
+    {
+        show_error($problem);
+    }
+    return $data;
+}
+
+function show_error($myError)
+{
+?>
+    <html>
+    <body>
+
+    <b>Please correct the following error:</b><br />
+    <?php echo $myError; ?>
+
+    </body>
+    </html>
+<?php
+exit();
+}
+?>
